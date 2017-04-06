6354f2a2c03f5f462ebdf64761674c321a016864
Luís Santos <luis@luissantos.org>
2017-04-06 21:41:53 +0100

    PDD_02

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 $(document).ready(function () {
                   
- $('.thumb2').on('click', function() {
-    console.log('CLICK');
-
+        $('.thumb2').on('click', function() {
+ 
               
         $('.modal-container').addClass('modal-isOpen');
      
     });
+    
+    
+    $(document).ready(function(){
+    $(".thumb2").click(function(){
+        $(".modal-container").width(1376).height(500);
+    });
+});
+    
+    
 
     $('.modal-close').on('click', function() {
         $('.modal-container').removeClass('modal-isOpen')
     });
     
 });
+
+console.log('hello');   
